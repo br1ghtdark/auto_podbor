ac3c84e897d7f175786e1699bfca25157fc122f5
br1ghtdark <marsha1l15@yandex.ru>
2023-08-22 23:34:06 +0300

    mobile adapt v.2

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ new Swiper('.swiper', {
     },
     speed: 1300,
 });
+
+
+// if (document.documentElement.clientWidth < 925) {
+//     let priceSlider = document.querySelector(".price__inner-container");
+//     priceSlider.classList.add("priceswiper");
+// } else {
+//     priceSlider.classList.remove("priceswiper");
+// };
+
+// new Swiper('.swiper', {
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+//     loop: false,
+//     slideClass: "price__content",
+//     speed: 1300,
+// });
